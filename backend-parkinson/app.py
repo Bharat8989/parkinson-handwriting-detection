140723be20d23e5c9b99ce48c2939d1b20e62c50
bharat8989 <kadamb208@gmail.com>
2026-04-09 17:58:18 +0530

    fix keras model loading

--- backend-parkinson/app.py
+++ backend-parkinson/app.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MODEL_PATH = os.path.join(BASE_DIR, "cnn_combined_model_final.keras")
 
-model = tf.keras.models.load_model(MODEL_PATH)
+model = tf.keras.models.load_model(MODEL_PATH, compile=False)
 
 class_names = ["healthy", "parkinson"]
 
